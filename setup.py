34bb7bd7880534adaff5de5f551fcaf46af4144b
adi18O3 <morningstar.sharma18@gmail.com>
2022-09-27 19:33:54 +0530

    logs and exception created

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 #Declaring variables for setup functions
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1"
-AUTHOR="Aditya Sharma"
-DESRCIPTION="This is a first  Machine Learning Project through pipelines"
-PACKAGES=["housing"]
+VERSION="0.0.3"
+AUTHOR="Avnish Yadav"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
+
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 
 def get_requirements_list()->List[str]:
     """
+   
     Description: This function is going to return list of requirement 
     mention in requirements.txt file
     return This function is going to return a list which contain name 
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 setup(
@@ -26,10 +27,6 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESRCIPTION,
-packages=PACKAGES,
+packages=find_packages(), 
 install_requires=get_requirements_list()
-
 )
-
-if __name__=="__main__":
-    print(get_requirements_list())
