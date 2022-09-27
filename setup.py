5f80f0b3cf0c17a2f712fc6358e0b5d099fa9405
adi18O3 <morningstar.sharma18@gmail.com>
2022-09-27 12:28:51 +0530

    files and folder structured completed

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from typing import List
+
+#Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.1"
+AUTHOR="Aditya Sharma"
+DESRCIPTION="This is a first  Machine Learning Project through pipelines"
+PACKAGES=["housing"]
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+
+def get_requirements_list()->List[str]:
+    """
+    Description: This function is going to return list of requirement 
+    mention in requirements.txt file
+    return This function is going to return a list which contain name 
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+
+
+setup(
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=PACKAGES,
+install_requires=get_requirements_list()
+
+)
+
+if __name__=="__main__":
+    print(get_requirements_list())
